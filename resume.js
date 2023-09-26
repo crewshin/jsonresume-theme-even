53752ad8e604add780a7e33d4bc3bf0d706937c2
Dmitry Semenov <dimitr.semenov@gmail.com>
2023-09-26 17:05:48 +0400

    Added print css

--- resume.js
+++ resume.js
@@ -21,7 +21,7 @@ export default function Resume(resume, css) {
         <meta charset="utf-8" />
         ${Meta(resume.basics)}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link media="print" href="./public/print.css" />
+        <link rel="stylesheet" href="public/print.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" />
         <style>
           ${css}
